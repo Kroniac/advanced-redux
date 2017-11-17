1f3fc0660a87dde17be7f5aa144dd9bf5a2337f0
Farid Ansari <farid12ansari7@gmail.com>
2017-11-18 05:25:26 +0530

    making switch leaner one line each by using fuctions

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -4,25 +4,33 @@ import { updateObject } from "../updateObject";
 const initialState = {
   result: []
 };
+
+const storeResult = (state, action) => {
+  return updateObject({
+    state,
+    result: state.result.concat({
+      id: new Date(),
+      value: action.result * 3
+    })
+  });
+};
+
+const deleteResult = (state, action) => {
+  //  const id =3;
+  //  const newArray =  [...state.result];
+  //  newArray.splice(id,1);
+  const updatedArray = state.result.filter(
+    result => result.id !== action.resultId
+  );
+  return updateObject({ state, result: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actiontype.STORE_RESULT:
-      return updateObject({
-        state,
-        result: state.result.concat({
-          id: new Date(),
-          value: action.result * 3
-        })
-      });
-
+      return storeResult(state, action);
     case actiontype.DELETE_RESULT:
-      //  const id =3;
-      //  const newArray =  [...state.result];
-      //  newArray.splice(id,1);
-      const updatedArray = state.result.filter(
-        result => result.id !== action.resultId
-      );
-      return updateObject({ state, result: updatedArray });
+      return deleteResult(state, action);
     default:
       return state;
   }
